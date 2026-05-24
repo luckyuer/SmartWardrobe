63617c6e976938f406d241bf5e4f86dc0b3278dd
yuhao <luckyuer@126.com>
2026-05-24 21:24:55 +0800

    fix: add Identifiable conformance to all Core Data entities

---
+++ SmartWardrobe/Models/CoreData+Identifiable.swift
@@ -0,0 +1,9 @@
+import Foundation
+import CoreData
+
+extension WardrobeItem: Identifiable {}
+extension Wardrobe: Identifiable {}
+extension Tag: Identifiable {}
+extension Outfit: Identifiable {}
+extension OutfitItem: Identifiable {}
+extension UsageRecord: Identifiable {}
